2cae56f16bad0c747c46693d23fb1d9d081d8c3f
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-06-24 18:08:54 +0100

    Improved ParseParamter slightly

--- ConsoleSnake/Program.cs
+++ ConsoleSnake/Program.cs
@@ -108,27 +108,32 @@ namespace ConsoleSnake
 			}
 			return false;
 		}
-		public static T ParseParameter<T>(string[] args, string paramName, T defaultValue = default)
+		public static T ParseParameter<T>(string[] args, string paramName, T defaultValue)
 		{
-			T result = defaultValue;
-			string valueStr;
-			for (int i = 0; i < args.Length; i++)
-			{
-				if (args[i].StartsWith("--" + paramName + "="))
-				{
-					valueStr = args[i].Substring(paramName.Length + 3);
-					try
-					{
-						result = (T)Convert.ChangeType(valueStr, typeof(T));
-						return result;
-					} catch
-					{
-						throw new ArgumentException($"{valueStr} was not of type {typeof(T)}, which was required by the --{paramName} parameter");
-					}
-				}
-			}
-			return result;
+			return ParseParameter<T>(args, paramName) ?? defaultValue;
 		}
+		public static T? ParseParameter<T>(string[] args, string paramName)
+		{
+			T? result = default;
+            string valueStr;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("--" + paramName + "="))
+                {
+                    valueStr = args[i].Substring(paramName.Length + 3);
+                    try
+                    {
+                        result = (T)Convert.ChangeType(valueStr, typeof(T));
+                        return result;
+                    }
+                    catch
+                    {
+                        throw new ArgumentException($"{valueStr} was not of type {typeof(T)}, which was required by the --{paramName} parameter");
+                    }
+                }
+            }
+            return result;
+        }
 
 		static void StartOld()
 		{
